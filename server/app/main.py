c7df30d42d6f48a6ffca06596eb72df955296256
alilotfy <alilotfy100@gmail.com>
2025-05-20 20:49:03 +0300

    auto register routers

--- server/app/main.py
+++ server/app/main.py
@@ -3,10 +3,10 @@ from fastapi import FastAPI
 
 from app.database import engine
 from app import  models
-from app.routers import athlete
+from app.routers import router as all_routers
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(athlete.router)
+app.include_router(all_routers)
---
+++ server/app/routers/__init__.py
@@ -0,0 +1,10 @@
+import pkgutil
+import importlib
+from fastapi import APIRouter
+
+router = APIRouter()
+
+for _, module_name, _ in pkgutil.iter_modules(__path__):
+    module = importlib.import_module(f"{__name__}.{module_name}")
+    if hasattr(module, "router"):
+        router.include_router(module.router)
