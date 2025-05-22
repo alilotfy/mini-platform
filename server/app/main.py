bac5267d05f7f28d741c0e1253a594304f416ef4
alilotfy <alilotfy100@gmail.com>
2025-05-22 16:01:41 +0300

    BE configure CORSMiddleware

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,5 @@
-# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app import  models
@@ -9,4 +9,11 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
 app.include_router(all_routers)
+
