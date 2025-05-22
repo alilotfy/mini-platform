c88cade2c11f7f7169b281d38c29052d494619c4
alilotfy <alilotfy100@gmail.com>
2025-05-22 22:20:35 +0300

    FE: show video upload date and improve video header style

--- client/src/app/services/video.service.ts
+++ client/src/app/services/video.service.ts
@@ -7,6 +7,7 @@ export interface Video {
   path: string;
   status: string;
   filename: string;
+  upload_date?: Date;
 }
 
 @Injectable({
--- server/app/schemas/video.py
+++ server/app/schemas/video.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import List
 from app.schemas.nested_athlete import NestedAthleteRead
+from datetime import datetime
 
 class VideoCreate(BaseModel):
     filename: str
@@ -9,7 +10,7 @@ class NestedVideoRead(VideoCreate):
     id: int
     path: str
     status: str
-
+    upload_date: datetime
     class Config:
         orm_mode = True
 
